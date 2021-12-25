9696bf79ebf715af6fa5c1e3f56efd05ed455f8d
Машина Паша <evtuh.pania@yandex.ru>
2021-12-25 23:10:01 +0800

    Прошёл первую методичку

---
+++ PlayWithParticles/Emitter.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PlayWithParticles
+{
+    internal class Emitter
+    {
+        public float GravitationX = 0;
+        public float GravitationY = 0; // пусть гравитация будет силой один пиксель за такт, нам хватит
+
+        List<Particle> particles = new List<Particle>();
+
+        public int MousePositionX;
+        public int MousePositionY;
+
+        public List<IImpactPoint> impactPoints = new List<IImpactPoint>();
+
+        public int ParticlesCount = 500;
+        public virtual void ResetParticle(Particle particle)
+        {
+            particle.Life = 20 + Particle.rand.Next(100);
+            particle.X = MousePositionX;
+            particle.Y = MousePositionY;
+
+            var direction = (double)Particle.rand.Next(360);
+            var speed = 1 + Particle.rand.Next(10);
+
+            particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+            particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+            particle.Radius = 2 + Particle.rand.Next(10);
+        }
+
+        public void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; // уменьшаю здоровье
+                                    // если здоровье кончилось
+                if (particle.Life < 0)
+                {
+                    ResetParticle(particle);
+                }
+                else
+                {
+                    // каждая точка по-своему воздействует на вектор скорости
+                    foreach (var point in impactPoints)
+                    {
+                        point.ImpactParticle(particle);
+                    }
+
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                    //var directionInRadians = particle.Direction / 180 * Math.PI;
+                    //particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    //particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < ParticlesCount) // пока частиц меньше ParticlesCount генерируем новые
+                {
+                    var particle = new Particle.ParticleColorful();
+
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+
+                    ResetParticle(particle);
+
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
+            }
+        }
+
+        public void Render(Graphics g)
+        {
+            // ну тут так и быть уж сам впишу...
+            // это то же самое что на форме в методе Render
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+
+            foreach (var point in impactPoints) // тут теперь  impactPoints
+            {
+                point.Render(g); // это добавили
+            }
+        }
+
+        public class TopEmitter : Emitter
+        {
+            public int Width; // длина экрана
+
+            public override void ResetParticle(Particle particle)
+            {
+                base.ResetParticle(particle); // вызываем базовый сброс частицы, там жизнь переопределяется и все такое
+
+                // а теперь тут уже подкручиваем параметры движения
+                particle.X = Particle.rand.Next(Width); // позиция X -- произвольная точка от 0 до Width
+                particle.Y = 0;  // ноль -- это верх экрана 
+
+                particle.SpeedY = 1; // падаем вниз по умолчанию
+                particle.SpeedX = Particle.rand.Next(-2, 2); // разброс влево и вправа у частиц 
+            }
+        }
+    }
+}
--- PlayWithParticles/Form1.cs
+++ PlayWithParticles/Form1.cs
@@ -8,16 +8,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace PlayWithParticles
 {
     public partial class Form1 : Form
     {
+        Emitter emitter;
+
         public Form1()
         {
             InitializeComponent();
-
-            // привязал изображение
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            emitter = new Emitter.TopEmitter
+            {
+                Width = picDisplay.Width,
+                GravitationY = 0.25f
+            };
+
+            //гравитон
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.25),
+                Y = picDisplay.Height / 2
+            });
+
+            // в центре антигравитон
+            emitter.impactPoints.Add(new AntiGravityPoint
+            {
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2
+            });
+
+            // снова гравитон
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.75),
+                Y = picDisplay.Height / 2
+            });
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            emitter.UpdateState();
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black); // А ЕЩЕ ЧЕРНЫЙ ФОН СДЕЛАЮ
+                emitter.Render(g);
+            }
+
+            picDisplay.Invalidate();
+
+        }
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
